893445a43571df7ef136f218d7101de8e6a570aa
15632703266 <liuyh_em@163.com>
2024-04-30 14:48:59 +0800

    完成简易框架搭建以及静态代理

---
+++ ex-provider/src/main/java/org/school/work/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package org.school.work;/**
+ * <p>Description: todo</p >
+ * <p>Copyright: Copyright (c)2024</p >
+ * <p>Company: todo</p >
+ * <P>Created Date: 2024年04月30日</P>
+ * @author LiuYuHan
+ * @version 1.0
+ */
+public class UserServiceImpl {
+}
---
+++ simple-rpc-easy/src/main/java/org/school/work/server/HttpServer.java
@@ -0,0 +1,17 @@
+package org.school.work;
+
+/**
+ * <p>Description: HTTP 服务器借口</p >
+ * <p>Copyright: Copyright (c)2024</p >
+ * <P>Created Date: 2024年04月30日</P>
+ *
+ * @author LiuYuHan
+ * @version 1.0
+ */
+public interface HttpServer {
+    /**
+     * 启动服务器
+     * @param port 端口
+     */
+    void doStart(int port);
+}
